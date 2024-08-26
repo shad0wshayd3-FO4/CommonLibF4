b13e4e63950e8e18ea31ce530a224f7bb4aa3a87
FalloutCascadia <54562300+FalloutCascadia@users.noreply.github.com>
2024-08-26 08:41:58 +0200

    Update TESForms.h

--- CommonLibF4/include/RE/Bethesda/TESForms.h
+++ CommonLibF4/include/RE/Bethesda/TESForms.h
@@ -910,7 +910,7 @@ namespace RE
 		std::uint32_t                            formFlags;        // 10
 		TESFormID                                formID;           // 14
 		std::uint16_t                            inGameFormFlags;  // 18
-		ENUM_FORM_ID							 formType;         // 1A
+		REX::EnumSet<ENUM_FORM_ID, std::uint8_t> formType;         // 1A
 	};
 	static_assert(sizeof(TESForm) == 0x20);
 

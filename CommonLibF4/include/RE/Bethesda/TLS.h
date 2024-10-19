57329624e3c3885d01260c9c5c9b9b95bff5ac4d
Qudix <17361645+Qudix@users.noreply.github.com>
2024-10-19 06:32:22 -0500

    feat: `TLS`

---
+++ CommonLibF4/include/RE/Bethesda/TLS.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "REX/W32/NT.h"
+
+namespace RE
+{
+	struct TLS
+	{
+		[[nodiscard]] static TLS* GetSingleton()
+		{
+			return *static_cast<TLS**>(REX::W32::NtCurrentTeb()->threadLocalStoragePointer);
+		}
+
+		// members
+		std::byte pad000[0x830];  // 000
+		bool      consoleMode;    // 830
+	};
+}

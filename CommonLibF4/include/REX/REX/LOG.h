f07a554760d0e1625e4be09379076ce267a1807d
Qudix <17361645+Qudix@users.noreply.github.com>
2025-03-18 01:11:24 -0500

    fix: support non format_string

--- CommonLibF4/include/REX/REX/LOG.h
+++ CommonLibF4/include/REX/REX/LOG.h
@@ -31,6 +31,17 @@ namespace REX
 		}
 	};
 
+	template <>
+	struct TRACE<void>
+	{
+		TRACE() = delete;
+
+		explicit TRACE(const std::string_view a_fmt, const std::source_location a_loc = std::source_location::current())
+		{
+			LOG(a_loc, LOG_LEVEL::TRACE, a_fmt);
+		}
+	};
+
 	template <class... T>
 	struct DEBUG
 	{
@@ -39,6 +50,17 @@ namespace REX
 		explicit DEBUG(const std::format_string<T...> a_fmt, T&&... a_args, const std::source_location a_loc = std::source_location::current())
 		{
 			LOG(a_loc, LOG_LEVEL::DEBUG, a_fmt, std::forward<T>(a_args)...);
+		}
+	};
+
+	template <>
+	struct DEBUG<void>
+	{
+		DEBUG() = delete;
+
+		explicit DEBUG(const std::string_view a_fmt, const std::source_location a_loc = std::source_location::current())
+		{
+			LOG(a_loc, LOG_LEVEL::DEBUG, a_fmt);
 		}
 	};
 
@@ -53,6 +75,17 @@ namespace REX
 		}
 	};
 
+	template <>
+	struct INFO<void>
+	{
+		INFO() = delete;
+
+		explicit INFO(const std::string_view a_fmt, const std::source_location a_loc = std::source_location::current())
+		{
+			LOG(a_loc, LOG_LEVEL::INFO, a_fmt);
+		}
+	};
+
 	template <class... T>
 	struct WARN
 	{
@@ -61,6 +94,17 @@ namespace REX
 		explicit WARN(const std::format_string<T...> a_fmt, T&&... a_args, const std::source_location a_loc = std::source_location::current())
 		{
 			LOG(a_loc, LOG_LEVEL::WARN, a_fmt, std::forward<T>(a_args)...);
+		}
+	};
+
+	template <>
+	struct WARN<void>
+	{
+		WARN() = delete;
+
+		explicit WARN(const std::string_view a_fmt, const std::source_location a_loc = std::source_location::current())
+		{
+			LOG(a_loc, LOG_LEVEL::WARN, a_fmt);
 		}
 	};
 
@@ -75,6 +119,17 @@ namespace REX
 		}
 	};
 
+	template <>
+	struct ERROR<void>
+	{
+		ERROR() = delete;
+
+		explicit ERROR(const std::string_view a_fmt, const std::source_location a_loc = std::source_location::current())
+		{
+			LOG(a_loc, LOG_LEVEL::ERROR, a_fmt);
+		}
+	};
+
 	template <class... T>
 	struct CRITICAL
 	{
@@ -86,32 +141,38 @@ namespace REX
 		}
 	};
 
-	template <class... T>
-	struct FAIL
+	template <>
+	struct CRITICAL<void>
 	{
-		FAIL() = delete;
+		CRITICAL() = delete;
 
-		explicit FAIL(const std::format_string<T...> a_fmt, T&&... a_args, const std::source_location a_loc = std::source_location::current())
+		explicit CRITICAL(const std::string_view a_fmt, const std::source_location a_loc = std::source_location::current())
 		{
-			LOG(a_loc, LOG_LEVEL::CRITICAL, a_fmt, std::forward<T>(a_args)...);
+			LOG(a_loc, LOG_LEVEL::CRITICAL, a_fmt);
 		}
 	};
 
 	template <class... T>
 	TRACE(const std::format_string<T...>, T&&...) -> TRACE<T...>;
+	TRACE(const std::string_view) -> TRACE<void>;
 
 	template <class... T>
 	DEBUG(const std::format_string<T...>, T&&...) -> DEBUG<T...>;
+	DEBUG(const std::string_view) -> DEBUG<void>;
 
 	template <class... T>
 	INFO(const std::format_string<T...>, T&&...) -> INFO<T...>;
+	INFO(const std::string_view) -> INFO<void>;
 
 	template <class... T>
 	WARN(const std::format_string<T...>, T&&...) -> WARN<T...>;
+	WARN(const std::string_view) -> WARN<void>;
 
 	template <class... T>
 	ERROR(const std::format_string<T...>, T&&...) -> ERROR<T...>;
+	ERROR(const std::string_view) -> ERROR<void>;
 
 	template <class... T>
 	CRITICAL(const std::format_string<T...>, T&&...) -> CRITICAL<T...>;
+	CRITICAL(const std::string_view) -> CRITICAL<void>;
 }

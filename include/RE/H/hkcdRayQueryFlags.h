4dc7180efc63031c71c8e93afb03ef66f216b059
FalloutCascadia <54562300+FalloutCascadia@users.noreply.github.com>
2025-06-12 13:31:58 +0200

    Update hkcdRayQueryFlags.h
    feat: Populate `hkcdRayQueryFlags::Enum` enum.

--- include/RE/H/hkcdRayQueryFlags.h
+++ include/RE/H/hkcdRayQueryFlags.h
@@ -5,7 +5,14 @@ namespace RE
 	class hkcdRayQueryFlags
 	{
 	public:
-		enum class Enum;
+		enum class Enum
+		{
+			kNoFlags = 0x0,
+			kDisableBackFacingTriangleHits = 0x1,
+			kDisableFrontFacingTriangleHits = 0x2,
+			kEnableInsideHits = 0x4,
+			kEnableExtraTriangleBevelplanes = 0x8
+		};
 	};
 	static_assert(std::is_empty_v<hkcdRayQueryFlags>);
 }
